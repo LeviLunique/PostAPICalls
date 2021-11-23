fd9544d91d1f2e82cac480dc8715ff78938b695d
Levi Lunique <levi.lunique@gmail.com>
2021-11-23 18:58:42 -0500

    Initial Commit

---
+++ PostAPICalls/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PostAPICalls
+//
+//  Created by user204006 on 11/23/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PostAPICalls/PostAPICallsApp.swift
@@ -0,0 +1,17 @@
+//
+//  PostAPICallsApp.swift
+//  PostAPICalls
+//
+//  Created by user204006 on 11/23/21.
+//
+
+import SwiftUI
+
+@main
+struct PostAPICallsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
