6ad04d3dce4c5896fff3144f40e19738fb1de333
Levi Lunique <levi.lunique@gmail.com>
2021-11-23 19:58:06 -0500

    Call and fetch JSON

---
+++ PostAPICalls/Data.swift
@@ -0,0 +1,30 @@
+//
+//  Data.swift
+//  PostAPICalls
+//
+//  Created by user204006 on 11/23/21.
+//
+
+import SwiftUI
+
+struct Post: Codable, Identifiable {
+    let id = UUID()
+    var title: String
+    var body: String
+}
+
+class Api {
+    func getPosts(completion: @escaping ([Post]) -> ()) {
+        guard let url = URL(string: "https://jsonplaceholder.typicode.com/posts") else { return }
+        
+        URLSession.shared.dataTask(with: url) { (data, _, _) in
+            let posts = try! JSONDecoder().decode([Post].self, from: data!)
+            
+            DispatchQueue.main.async {
+                completion(posts)
+            }
+            
+        }
+        .resume()
+    }
+}
